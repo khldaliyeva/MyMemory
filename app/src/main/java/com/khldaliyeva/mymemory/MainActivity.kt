1ba27b345f10d7123294e10b0c0ca228f63778ed
Khalida Aliyeva <khld.aliyeva@gmail.com>
2022-08-18 17:29:45 +0400

    Set up MainActivity scaffolding and define references to key views

--- app/src/main/java/com/khldaliyeva/mymemory/MainActivity.kt
+++ app/src/main/java/com/khldaliyeva/mymemory/MainActivity.kt
@@ -2,10 +2,21 @@ package com.khldaliyeva.mymemory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvBoard: RecyclerView
+    private lateinit var tvNumMoves: TextView
+    private lateinit var tvNumPairs: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvBoard = findViewById(R.id.rvBoard)
+        tvNumMoves = findViewById(R.id.tvNumMoves)
+        tvNumPairs = findViewById(R.id.tvNumPairs)
     }
 }
